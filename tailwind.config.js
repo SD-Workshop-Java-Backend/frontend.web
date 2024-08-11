7ce0f3869e8b7f1cc89c39c1b99bc2dd1014d3bf
Arthur <arthurferreira1417@gmail.com>
2024-08-11 14:07:35 -0300

    Faet: adicioando animações de carregamento

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { transform } = require("next/dist/build/swc");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,6 +9,18 @@ module.exports = {
   ],
   theme: {
     extend: {
+      keyframes: {
+        loading: {
+          "20%": { transform: "scaleY(1.2)" },
+          "40%": { transform: "scaleY(1)" },
+        },
+      },
+      animation: {
+        loading: "loading  1s ease infinite",
+        "loading-100" : "loading 1s  ease 0.1s infinite",
+        "loading-200" : "loading 1s  ease 0.25s infinite",
+        "loading-300" : "loading 1s  ease 0.5s infinite",
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
