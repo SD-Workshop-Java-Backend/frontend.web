a27f03fb8173dac04b543b8f1babb27e315a5de6
Arthur <arthurferreira1417@gmail.com>
2024-08-11 14:08:22 -0300

    Faet: adicioando responsividade e melhoria na foto

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,11 +1,15 @@
 // import Imge from "../assets/woman.jpg";
 // import HomeImage from "@/components/images/HomeImage";
+"use server";
+import HomeImage from "@/components/HomeImage";
+import SkelletonImage from "@/components/skelleton/Image";
+import { Suspense } from "react";
 
-export default function Home() {
+export default async function Home() {
   return (
     <main className="flex min-h-screen flex-row items-center justify-between ">
-      <div className="flex flex-col items-center justify-start  gap-12 w-2/5 h-screen">
-        <div className="flex flex-col gap-1 w-2/3  items-start  mt-24">
+      <div className="flex flex-col items-center justify-center  sm:max-md:gap-6 md:max-lg:gap-8 gap-12 max-md:w-full w-2/5 h-screen">
+        <div className="flex flex-col gap-1  w-2/3  items-start mt-10 sm:mt-12  md:mt-16  ">
           <span className="text-3xl font-bold select-none ">
             {" "}
             Bem-vindo de volta
@@ -19,20 +23,22 @@ export default function Home() {
         <div className="flex flex-col w-full gap-3 items-center justify-center">
           <div className="flex flex-col gap-12 items-center  w-full">
             <input
-              type="text"
+              type="email"
               placeholder="Email"
               className="outline-none text-sm border-b w-2/3  border-gray-400 focus:border-gray-600 transition-all duration-100 ease-linear p-2"
             />
             <input
               type="password"
               placeholder="Senha"
-              className="outline-none text-sm border-b w-2/3 border-gray-400 focus:border-gray-600 transition-all duration-100 ease-linear py-2"
+              className="outline-none text-sm border-b w-2/3 border-gray-400 focus:border-gray-600 transition-all duration-100 ease-linear p-2"
             />
           </div>
-          <span className="text-blue-700 text-xs  w-2/3 cursor-pointer">Esqueceu?</span>
+          <span className="text-blue-700 text-xs  w-2/3 cursor-pointer">
+            Esqueceu?
+          </span>
         </div>
 
-        <div className=" flex flex-col items-center justify-center w-full mt-10 gap-5">
+        <div className=" flex flex-col items-center justify-center w-full  mt-10 gap-5 lg:mb-16">
           <button className="w-2/3 h-11 text-white rounded-lg bg-black  p-2">
             Entrar
           </button>
@@ -42,16 +48,9 @@ export default function Home() {
           </div>
         </div>
       </div>
-      <div className=" h-screen w-3/4 py-5 ">
-        <img
-          className="w-full h-full object-contain  "
-          src="/images/gradient.png"
-        />
-        <span className="text-xl text-white absolute bottom-24 pl-8  select-none">
-          Encontre um mundo de possibilidades
-          <br /> feito exclusivamente para você.
-        </span>
-      </div>
+      <Suspense fallback={<SkelletonImage/>}>
+        <HomeImage />
+      </Suspense>
     </main>
   );
 }
